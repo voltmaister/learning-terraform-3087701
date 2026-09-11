e6b5bb42e628e162754dd5989b182666d2ef1311
voltmaister <45509916+voltmaister@users.noreply.github.com>
2026-09-11 14:19:37 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,30 @@
 data "aws_ami" "app_ami" {
   most_recent = true
+  owners      = ["099720109477"] # Canonical (Official Ubuntu)
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
-  owners = ["979382823631"] # Bitnami
 }
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
+
+  # This script installs Tomcat and Java automatically on first boot
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y default-jdk tomcat10
+              systemctl enable tomcat10
+              systemctl start tomcat10
+              EOF
 
   tags = {
     Name = "HelloWorld"
