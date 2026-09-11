972d99b110fbf7965784ecdefc8c8b6cf256e687
voltmaister <45509916+voltmaister@users.noreply.github.com>
2026-09-11 15:09:58 +0300

    Security group

--- main.tf
+++ main.tf
@@ -13,11 +13,16 @@ data "aws_ami" "app_ami" {
   }
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   
-  # Update this to t3.micro
   instance_type = var.instance_type
+
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   # This script installs Tomcat and Java automatically on first boot
   user_data = <<-EOF
@@ -31,4 +36,41 @@ resource "aws_instance" "blog" {
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "blog" {
+  name        = "blog"
+  description = "Allow Http and Https in. allow all out"
+
+  vpc_id      = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
 }
